39b9ccf170ff9ff0d010f89c16a4c23f682a6d84
= <=>
2016-10-07 11:16:25 +0530

    Initial commit

---
+++ shortcutkey/Form1.Designer.vb
@@ -0,0 +1,93 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.timer_enter = New System.Windows.Forms.Timer(Me.components)
+        Me.lbl_state = New System.Windows.Forms.Label()
+        Me.lbl_showstate = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'timer_enter
+        '
+        '
+        'lbl_state
+        '
+        Me.lbl_state.AutoSize = True
+        Me.lbl_state.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lbl_state.Location = New System.Drawing.Point(169, 107)
+        Me.lbl_state.Name = "lbl_state"
+        Me.lbl_state.Size = New System.Drawing.Size(97, 32)
+        Me.lbl_state.TabIndex = 0
+        Me.lbl_state.Text = "State :"
+        '
+        'lbl_showstate
+        '
+        Me.lbl_showstate.AutoSize = True
+        Me.lbl_showstate.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lbl_showstate.Location = New System.Drawing.Point(272, 107)
+        Me.lbl_showstate.Name = "lbl_showstate"
+        Me.lbl_showstate.Size = New System.Drawing.Size(71, 32)
+        Me.lbl_showstate.TabIndex = 1
+        Me.lbl_showstate.Text = "OFF"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 13.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.DarkOliveGreen
+        Me.Label1.Location = New System.Drawing.Point(40, 37)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(522, 29)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Press F10 to Turn On and Press F11 to Turn Off"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(617, 275)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.lbl_showstate)
+        Me.Controls.Add(Me.lbl_state)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Margin = New System.Windows.Forms.Padding(4)
+        Me.MaximizeBox = False
+        Me.MaximumSize = New System.Drawing.Size(635, 322)
+        Me.MinimumSize = New System.Drawing.Size(635, 322)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Auto Join Game Clicker"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents timer_enter As Timer
+    Friend WithEvents lbl_state As Label
+    Friend WithEvents lbl_showstate As Label
+    Friend WithEvents Label1 As Label
+End Class
---
+++ shortcutkey/Form1.vb
@@ -0,0 +1,39 @@
+﻿Public Class Form1
+
+    <System.Runtime.InteropServices.DllImport("user32")> Private Shared Function GetAsyncKeyState(ByVal vKey As Integer) As Integer
+    End Function
+
+    WithEvents Timer As New System.Windows.Forms.Timer
+
+    Private Sub Form1_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+
+        Timer.Enabled = True
+        Timer.Interval = 50
+        Timer.Start()
+    End Sub
+    Private Sub TimerTick(ByVal sender As Object, ByVal e As System.EventArgs) Handles Timer.Tick
+        '  If CBool(GetAsyncKeyState(Keys.Alt)) And CBool(GetAsyncKeyState(Keys.A)) = True Then
+        '    Microsoft.VisualBasic.Interaction.MsgBox("a & alt button was pressed.")
+        If CBool(GetAsyncKeyState(Keys.F10)) Then
+            'start it
+            lbl_showstate.ForeColor = Color.Green
+            lbl_showstate.Text = "STARTED"
+            timer_enter.Enabled = True
+            timer_enter.Interval = 120
+            timer_enter.Start()
+
+
+        ElseIf CBool(GetAsyncKeyState(Keys.F11)) Then
+            'Stop it
+            lbl_showstate.ForeColor = Color.Red
+            lbl_showstate.Text = "STOPPED"
+            timer_enter.Enabled = False
+            timer_enter.Stop()
+        End If
+    End Sub
+
+    Private Sub timer_enter_Tick(sender As Object, e As EventArgs) Handles timer_enter.Tick
+        LeftClick()
+
+    End Sub
+End Class
